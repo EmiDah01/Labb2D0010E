7177a8f278ecb22d95e5fb9a7b45d94562b1d403
EmiDah01 <78493372+EmiDah01@users.noreply.github.com>
2021-02-08 22:09:56 +0100

    Update Driver.java

--- src/lab2/Driver.java
+++ src/lab2/Driver.java
@@ -1,14 +1,32 @@
 package lab2;
 
-import lab2.level.Level;
+import java.awt.Color;
 import lab2.level.LevelGUI;
+import lab2.level.Level;
 import lab2.level.Room;
 
 public class Driver {
 
-	public void run() 
-	{
-		lab2.level.Level lv = new lab2.level.Level();
-	}
+	public void run() {
+	
+		Room r1 = new Room(150, 150, Color.red);
+		Room r2 = new Room(150, 150, Color.green);
+		Room r3 = new Room(150, 150, Color.cyan);
+		Room r4 = new Room(150, 150, Color.orange);
+		Room r5 = new Room(150, 150, Color.yellow);
+		
+		r1.connectEastTo(r2);
+		r2.connectWestTo(r1);
+		
+		r3.connectNorthTo(r1);
+		r1.connectSouthTo(r3);
+		
+		r3.connectSouthTo(r4);
+		r4.connectNorthTo(r3);
+		
+		r5.connectWestTo(r2);
+		r2.connectEastTo(r1);
+
+    }
 
 }
